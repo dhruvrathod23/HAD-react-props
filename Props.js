a393592387f431ea66fe279250331ed8c235f393
dhruv rathod <62165682+dhruvrathod23@users.noreply.github.com>
2021-02-01 15:26:25 +0530

    Add files via upload

---
+++ Props.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import User from './User'
+const Props = () => {
+    return (
+        <div>
+            <h4>THIS IS  PROPS COMPONENT</h4>
+            <User NAME = "RAJ" />
+            <User NAME = "DHRUV " />
+            <User NAME = "SHYAM" />
+            <User NAME = "VISHAL" />
+            
+        </div>
+    )
+}
+
+export default Props
---
+++ User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = (props) => {
+    return (
+        <div>
+            <p>USER NAME : {props.NAME}</p>
+        </div>
+    )
+}
+
+export default User
